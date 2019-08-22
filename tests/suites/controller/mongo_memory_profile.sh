1578f031e5dfcd306196378d813f4d7369c56f15
Tim Penhey <tim@penhey.net>
2019-08-22 12:07:27 +1200

    Clean up the test.

--- tests/suites/controller/mongo_memory_profile.sh
+++ tests/suites/controller/mongo_memory_profile.sh
@@ -1,34 +1,23 @@
+cat_mongo_service() {
+    echo $(juju run -m controller --machine 0 'cat /lib/systemd/system/juju-db/juju-db.service' | grep "^ExecStart")
+}
+
 run_mongo_memory_profile() {
     echo
 
     file="${TEST_DIR}/mongo_memory_profile.txt"
 
-    echo ${file}
-
     ensure_controller "mongo-memory-profile" "${file}"
 
-    LINE=$(juju run -m controller --machine 0 'cat /lib/systemd/system/juju-db/juju-db.service' | grep "^ExecStart")
-    CHK=$(echo "${LINE}" | grep wiredTigerCacheSizeGB || true)
-    if [ -n "${CHK}" ]; then
-        printf "unexpected wiredTigerCacheSizeGB found in output\n\n%s\n" "${LINE}"
-        exit 1
-    else
-        printf "success: wiredTigerCacheSizeGB not found in output\n"
-    fi
+    check_contains "$(cat_mongo_service)" wiredTigerCacheSizeGB
 
     juju controller-config mongo-memory-profile=low
 
     sleep 5
 
-    LINE=$(juju run -m controller --machine 0 'cat /lib/systemd/system/juju-db/juju-db.service' | grep "^ExecStart")
-    CHK=$(echo "${LINE}" | grep wiredTigerCacheSizeGB || true)
-    if [ -n "${CHK}" ]; then
-        printf "success: wiredTigerCacheSizeGB found as expected\n"
-    else
-        printf "wiredTigerCacheSizeGB arg missing from output\n\n%s\n" "${LINE}"
-        exit 1
-    fi
+    check_not_contains "$(cat_mongo_service)" wiredTigerCacheSizeGB
 
+    # Set the value back in case we are reusing a controller
     juju controller-config mongo-memory-profile=default
 
     sleep 5
